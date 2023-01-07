91439717be3e416a537a95b37d937ba85230e563
Andrey Ignatov <ando7667@gmail.com>
2023-01-07 04:35:14 +0300

    Доашняя работа к семинару 8

---
+++ src/Lesson_10/Ex005/Ex004_GenericTest.java
@@ -0,0 +1,11 @@
+
+package Lesson_10.Ex005;
+
+public class Ex004_GenericTest {
+    public static void main(String[] args) {
+        // T type
+        // E element
+        // K key
+        // V value
+    }
+}
---
+++ src/org/geekbrains/Main.java
@@ -0,0 +1,9 @@
+package org.geekbrains;
+
+public class Main {
+    // имя файла должно совпадать с именем класса (public)
+    // классы называются в стиле CamelCase
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
