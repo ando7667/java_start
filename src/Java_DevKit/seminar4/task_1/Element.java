8a5ccd1b87b0afd39b1a490b9ba824728a9a22c7
Andrey Ignatov <ando7667@gmail.com>
2023-11-02 03:14:33 +0300

    Семинар 4

---
+++ src/Java_DevKit/seminar4/task_1/Element.java
@@ -0,0 +1,19 @@
+package Java_DevKit.seminar4.task_1;
+
+public class Element{
+    private Object element;
+    private Element nextElement;
+    private Element prevElement;
+
+    public Element getNext(){
+        return this.nextElement;
+    }
+
+    public Element getPreveus(){
+        return this.prevElement;
+    }
+
+    public void set(Object o){
+        element = o;
+    }
+}
