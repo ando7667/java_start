d53aa800696f6876c002404ce3d39d4fa0afaf0b
Andrey Ignatov <ando7667@gmail.com>
2023-11-02 03:03:21 +0300

    домашняя работа к уроку 4. Коллекции

--- src/Java_DevKit/HW4/Main.java
+++ src/Java_DevKit/HW4/Main.java
@@ -18,7 +18,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
     public static int getNumber() {
         int num;
         if (scanner.hasNextInt()) {
